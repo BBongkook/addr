8ae12e93f20d39d659b1286e2c5f005c701d1ed9
BBongkook <Administrator@USER-PC>
2019-04-03 17:29:44 +0900

    4월 3일

--- src/com/addr/Exam01.java
+++ src/com/addr/Exam01.java
@@ -1,18 +1,26 @@
-package com.addr.db;
+package com.addr;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.addr.dao.AddressDAO;
+import com.addr.dao.impl.AddressDAOImpl;
+
 public class Exam01 {
 	private static final String ADDR_FILE_PATH ="D:\\study\\addr\\jibun_chungnam.txt";
+	private static AddressDAO adao = new AddressDAOImpl();
+	
 	public static void main(String[] args) {
+		Long sTime = System.currentTimeMillis();
+		
 		File f = new File(ADDR_FILE_PATH);
 		List<String> colList = new ArrayList<>();
 		colList.add("ad_code");
@@ -34,14 +42,26 @@ public class Exam01 {
 			BufferedReader br = new BufferedReader(fr);
 			String line = "";
 			List<Map<String,String>> addrList = new ArrayList<>();
-			while((line=br.readLine())!=null) {
+			int cnt = 1;
+			while((line=br.readLine())!=null) {	
 				String[] lines = line.split("\\|");
 				Map<String,String> addrMap = new HashMap<>();
 				for(int i=0; i<lines.length; i++) {
-					addrMap.put(colList.get(i), lines[i]);
+					addrMap.put(colList.get(i), lines[i]);	
 				}
 				addrList.add(addrMap);
+				if(addrList.size()==1000) {
+					Long subStime = System.currentTimeMillis();
+					int result = adao.insertAddressList(addrList);
+					addrList.clear();
+					System.out.println("반영된 건수 : " + result);
+					System.out.println("1000개 insert 완료시간 : "+(System.currentTimeMillis()-subStime));
+				}	
 			}
+			Long subStime = System.currentTimeMillis();
+			int result = adao.insertAddressList(addrList);
+			System.out.println("반영된 건수 : " + result);
+			System.out.println("나머지 insert 완료시간 : "+(System.currentTimeMillis()-subStime));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -49,5 +69,6 @@ public class Exam01 {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		System.out.println("전체 수행 시간 : " + (System.currentTimeMillis()- sTime));
 	}
 }
---
+++ src/com/addr/db/DBCon.java
@@ -0,0 +1,68 @@
+package com.addr.db;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
+public class DBCon {
+	private static final String URL;
+	private static final String USER;
+	private static final String PASSWORD;
+	private static final String DRIVER;
+	public static final String CLIENID;
+	public static final String CLIENTSECRET;
+	public static final String NAVERURL;
+	public static final String METHOD;
+	private static Connection con = null;
+	static {
+		InputStream is = DBCon.class.getResourceAsStream("/com/addr/config/db.properties");
+		Properties prop = new Properties();
+			try {
+				prop.load(is);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			URL = prop.getProperty("url");
+			USER = prop.getProperty("user");
+			PASSWORD = prop.getProperty("password");
+			DRIVER = prop.getProperty("classname");
+			CLIENID = prop.getProperty("clientId");
+			CLIENTSECRET = prop.getProperty("clientSecret");
+			NAVERURL = prop.getProperty("naverUrl");
+			METHOD = prop.getProperty("method");
+	}	
+	
+	
+	public static Connection openCon() {
+		if(con==null) {
+			try {
+				Class.forName(DRIVER);
+				con = DriverManager.getConnection(URL, USER, PASSWORD);
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return con;
+	}
+	public static void close() {
+		if(con!=null) {
+			try {
+				if(!con.isClosed()) {
+					con.close();
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		con = null;
+	}
+}
